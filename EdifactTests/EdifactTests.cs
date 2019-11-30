c75893776cb200c9f4d75d4772a46f998d9f6c41
sanja <sanjay.gurjar87@gmail.com>
2019-11-30 10:25:03 +0000

    Add project files.

---
+++ EdifactTests/EdifactTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EdifactMessageParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edifact.Tests
+{
+    [TestClass()]
+    public class EdifactTests
+    {
+        [TestMethod()]
+        public void EdifactTest()
+        {
+            string[] args = new string[] { };
+            Program.Main(args);
+            Assert.IsFalse(false);
+        }
+    }
+}
